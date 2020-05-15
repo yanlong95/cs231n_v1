ce1040a5a98d95235a058863172bcc266a144649
zjackli <51221338+zjackli@users.noreply.github.com>
2020-05-15 14:51:36 -0700

    Update osm_generate_building_centroid.py
    add functionality to write csv based on building type and export in a group
    add functionality to randomly pick certain number of building based on building type

--- osm_generate_building_centroid.py
+++ osm_generate_building_centroid.py
@@ -3,17 +3,20 @@
 OSM file viewer
 Obtain OSM building nodal coordinate and save in framed structure
 Calculate centroid of the building footprint polygon and export as csv based on building id
+
 docs: https://docs.osmcode.org/pyosmium/latest/index.html
 
-Required package: numpy, panda, matplotlib, osmium, csv
+Required package: panda, matplotlib, osmium, csv, random
+
 Created on Tue May 5 11:05:21 2020
+
 @author: Jack Li
 """
 
 import osmium as osm
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import random
 import csv
 
 # define handler for different elements in OSM file
@@ -33,16 +36,40 @@ class OsmHandler(osm.SimpleHandler):
                               n.location.x,
                               len(n.tags)
                               ])
+
     def way(self, w):
-       if w.is_closed() and ("building" in w.tags): # check if is building footprint polygon
-            for anode in w.nodes:
-                self.osm_data_way.append(["way",
-                                  w.id,                 # building id
-                                  anode.ref,            # edge node id
-                                  anode.location.y,     # edge node lat
-                                  anode.location.x,     # edge node lon
-                                  len(w.tags)           # num of tags
-                                  ])
+        
+        # only check building with these tags on
+        category_filter = ['apartments', 
+                           'church', 
+                           'garage', 
+                           'house', 'detached', 'bungalow', 'semidetached_house', 'villa',
+                           'industrial', 'warehouse',
+                           'office', 
+                           'retail',
+                           'hotel',
+                           'roof',
+                           #'yes' # this is selected only if the filter can't generate enough building
+                           ]
+        
+        if w.is_closed() and (w.tags.get('building') in category_filter): # check if is building footprint polygon
+             for anode in w.nodes:
+                 self.osm_data_way.append(["way",
+                                   w.id,                 # building id
+                                   anode.ref,            # edge node id
+                                   anode.location.y,     # edge node lat
+                                   anode.location.x,     # edge node lon
+                                   len(w.tags),           # num of tags
+                                   w.tags.get('building')         
+                                   ])
+
+    # def relation(self, r):
+    #     self.osm_data_relation.append(["relation",
+    #                           r.id,
+    #                           r.visible,
+                              
+    #                           len(r.tags)
+    #                           ])
 
 # find centroid of a non self-interacting closed polygon by n nodes
 # @ param pt_x / y_list: list of lat and lon values
@@ -66,61 +93,74 @@ def find_centroid_polygon(pt_x_list, pt_y_list):
     c_y = int(f_y / (A * 6))
     return c_x, c_y
 
+# generate dataframe containing building id and centroid coordinate
+# @ param df: raw dataframe from osm file
+# @ return polygon_centroid_df: dataframe with building id and centroid coordinate
+def generate_centroid_dataframe(df):
+    node_lat_list = []
+    node_lon_list = []
+    polygon_list = {}
+    checker_pre = 0
+    checker = 0
+    # group node lat and lon based on polygon id
+    for index, row in df_osm_way.iterrows():
+        checker = row['id']
+        if (checker_pre == checker):
+            node_lat_list.append(row['node_lat'])
+            node_lon_list.append(row['node_lon'])
+            polygon_list[row['id']] = [node_lat_list, node_lon_list]
+        else:
+            node_lat_list = []
+            node_lon_list = []
+            node_lat_list.append(row['node_lat'])
+            node_lon_list.append(row['node_lon'])
+            polygon_list[row['id']] = [node_lat_list, node_lon_list]  
+        checker_pre = checker
+
+    # calculate polygon centroid and save it as dictionary per building id
+    polygon_centroid_list = {}
+    for key in polygon_list:
+        c_x, c_y = find_centroid_polygon(polygon_list[key][0], polygon_list[key][1])
+        polygon_centroid_list[key] = [c_x, c_y]
+    
+    polygon_centroid_df = pd.DataFrame.from_dict(polygon_centroid_list, orient='index')
+    
+    return polygon_centroid_df
+
+
 ######################## main ############################
 
 osmhandler = OsmHandler()
 # scan the input file and fills the handler list accordingly
-PATH = "C:/Users/jack_minimonster/Documents/231n_dataset/BIC_GSV/data_query/"
-filename = "vancouver_test_region_osm.osm"
+PATH = "C:/Users/jack_minimonster/Documents/231n_dataset/BIC_GSV/data_query/osm_file/"
+filename = "georgia.osm.pbf"
 
 osmhandler.apply_file(PATH + filename, locations=True) # set location to True to get coordinate
 
-# data_colnames_node = ['type', 'id', 'visible',
-#                   'lat', 'lon', 'ntags']
-# df_osm_node = pd.DataFrame(osmhandler.osm_data_node, columns=data_colnames_node)
-# df_osm_node = df_osm_node.sort_values(by=['type', 'id'])
-
 # extract building footprint information from osm and sort based on building id
 data_colnames_way = ['type', 'id',
-                  'edge_node_id', 'node_lat', 'node_lon', 'ntags']
+                  'edge_node_id', 'node_lat', 'node_lon', 'ntags', 'building type']
 df_osm_way = pd.DataFrame(osmhandler.osm_data_way, columns=data_colnames_way)
 df_osm_way = df_osm_way.sort_values(by=['type', 'id'])
 
-node_lat_list = []
-node_lon_list = []
-polygon_list = {}
-checker_pre = 0
-checker = 0
+df_osm_grouped = df_osm_way.groupby('building type')
 
-# group node lat and lon based on polygon id
-for index, row in df_osm_way.iterrows():
-    
-    checker = row['id']
-    if (checker_pre == checker):
-        node_lat_list.append(row['node_lat'])
-        node_lon_list.append(row['node_lon'])
-        polygon_list[row['id']] = [node_lat_list, node_lon_list]
-    else:
-        node_lat_list = []
-        node_lon_list = []
-        node_lat_list.append(row['node_lat'])
-        node_lon_list.append(row['node_lon'])
-        polygon_list[row['id']] = [node_lat_list, node_lon_list]
-    
-    checker_pre = checker
+for group_name, group_value in df_osm_grouped:
+    df_osm_building_group = df_osm_grouped.get_group(group_name)
+    polygon_centroid_df = generate_centroid_dataframe(df_osm_building_group)
 
-# calculate polygon centroid and save it as dictionary per building id
-polygon_centroid_list = {}
-for key in polygon_list:
-    c_x, c_y = find_centroid_polygon(polygon_list[key][0], polygon_list[key][1])
-    polygon_centroid_list[key] = [c_x, c_y]
+    # randomly pick certain number of examples from the full building list
+    polygon_centroid_df_sample = polygon_centroid_df.sample(n=10)
+    csvname = filename[:-8] + '-' + str(group_name)
 
-# save the centroid lat lon information as csv file
+    # # save the centroid lat lon information as csv file
+    print('...saving csv - ' + csvname + '...')
+    polygon_centroid_df_sample.to_csv(csvname + '.csv')
 
-csv_filename = 'building_centroid_coord.csv'
-with open(csv_filename, 'w') as csv_file:
-    csv_file.write("building_id,centroid_lat,centroid_lon\n")
-    for key in polygon_centroid_list.keys():
-        csv_file.write("%s,%s,%s\n"%(key,polygon_centroid_list[key][0],polygon_centroid_list[key][1]))
+# csv_filename = 'building_centroid_coord.csv'
+# with open(csv_filename, 'w') as csv_file:
+#     for key in polygon_centroid_list_sample.keys():
+#         csv_file.write("%s,%s,%s\n"%(key,polygon_centroid_list_sample[key][0],
+#                                      polygon_centroid_list_sample[key][1]))
 
-# output csv file in format 'building_id, [centroid lat x 1e7, centroid lon x 1e7]
+# # output csv file in format 'building_id, [centroid lat x 1e7, centroid lon x 1e7]
