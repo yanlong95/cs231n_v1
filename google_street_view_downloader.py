2a71b112e1caa0d106901e221f8e6b6c9d4e086a
zjackli <51221338+zjackli@users.noreply.github.com>
2020-05-15 14:52:08 -0700

    Update google_street_view_downloader.py
    bug fix

--- google_street_view_downloader.py
+++ google_street_view_downloader.py
@@ -17,14 +17,15 @@ import google_streetview.helpers as gsvhelpers
 import csv
 #from decimal import Decimal
     
-gsv_api_key = 'AIzaSyDHDDb152DY6d7bHjz-DEbrNOptudfad6U' # insert your own google street view static API
+gsv_api_key = 'AIzaSyDlpR8QBNNMHFXFoQol0zVbMpt_aLrkdeM' # insert your own google street view static API
 
-csv_filename = 'building_centroid_coord.csv'
+csv_filename = 'centroid_csv_file/georgia_apartment.csv'
 # csv file is in format of 'building id, centroid lat, centroid lon'
 
 location_str = ''
 with open(csv_filename) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader) # skip the header
     for row in csv_reader:
         centroid_lat = str(row[1])
         centroid_lon = str(row[2])
@@ -32,23 +33,24 @@ with open(csv_filename) as csv_file:
         # to obtain the actual coord needs to divide by 1e7
         location_str += centroid_lat[:-7] + '.' + centroid_lat[-7:] + ',' + centroid_lon[:-7] + '.' + centroid_lon[-7:] + ';'
 
+print('...reading parameters...')
 params = {
   'size': '512x512', # max 640x640 pixels
-  'location': location_str,
+  'location': location_str[:-1],
   #'heading': '0;30;60;90;120;150;180;210;240;270;300;330', # if don't specify heading it will face the coordinate point
   'pitch': '10',
-  'key': gsv_api_key
+  'key': gsv_api_key,
+  'source': 'outdoor'
 }
 
 # Create a results object
 street_view_list = gsvhelpers.api_list(params)
-print('...reading parameters...')
+print('...data querying from API...')
 results = gsvapi.results(street_view_list)
 
 # Download images to directory 'downloads'
-print('...data querying from API...')
-results.preview()
+# results.preview()
 print('...saving images...')
-results.download_links('street_view_downloads')
+results.download_links(csv_filename[17:-4])
 print('...saving metadata...')
 results.save_metadata('street_view_metadata.json')
