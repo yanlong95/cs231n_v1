87de1354720b314f4fc405ea249e44cece5b776d
zjackli <51221338+zjackli@users.noreply.github.com>
2020-05-17 13:53:41 -0700

    Update osm_generate_building_centroid.py
    bug fix

--- osm_generate_building_centroid.py
+++ osm_generate_building_centroid.py
@@ -16,8 +16,8 @@ Created on Tue May 5 11:05:21 2020
 import osmium as osm
 import pandas as pd
 import matplotlib.pyplot as plt
-import random
 import csv
+import random
 
 # define handler for different elements in OSM file
 # @param osm.
@@ -43,11 +43,16 @@ class OsmHandler(osm.SimpleHandler):
         category_filter = ['apartments', 
                            'church', 
                            'garage', 
-                           'house', 'detached', 'bungalow', 'semidetached_house', 'villa',
-                           'industrial', 'warehouse',
+                           'house', 
+                           #'detached', 
+                           #'bungalow', 
+                           #'semidetached_house', 
+                           #'villa',
+                           'industrial', 
+                           'warehouse',
                            'office', 
                            'retail',
-                           'hotel',
+                           #'hotel',
                            'roof',
                            #'yes' # this is selected only if the filter can't generate enough building
                            ]
@@ -93,17 +98,17 @@ def find_centroid_polygon(pt_x_list, pt_y_list):
     c_y = int(f_y / (A * 6))
     return c_x, c_y
 
-# generate dataframe containing building id and centroid coordinate
+# convert polygon nodal information from dataframe to dictionary format
 # @ param df: raw dataframe from osm file
-# @ return polygon_centroid_df: dataframe with building id and centroid coordinate
-def generate_centroid_dataframe(df):
+# @ return polygon_list: dictionary with building id and centroid coordinate
+def polygon_node_dataframe_to_dict(df):
     node_lat_list = []
     node_lon_list = []
     polygon_list = {}
     checker_pre = 0
     checker = 0
     # group node lat and lon based on polygon id
-    for index, row in df_osm_way.iterrows():
+    for index, row in df.iterrows():
         checker = row['id']
         if (checker_pre == checker):
             node_lat_list.append(row['node_lat'])
@@ -116,11 +121,17 @@ def generate_centroid_dataframe(df):
             node_lon_list.append(row['node_lon'])
             polygon_list[row['id']] = [node_lat_list, node_lon_list]  
         checker_pre = checker
+    
+    return polygon_list
 
+# generate dataframe containing building id and centroid coordinate
+# @ param df: raw dataframe from osm file
+# @ return polygon_centroid_df: dataframe with building id and centroid coordinate
+def generate_centroid_dataframe(input_dict):
     # calculate polygon centroid and save it as dictionary per building id
     polygon_centroid_list = {}
-    for key in polygon_list:
-        c_x, c_y = find_centroid_polygon(polygon_list[key][0], polygon_list[key][1])
+    for key in input_dict:
+        c_x, c_y = find_centroid_polygon(input_dict[key][0], input_dict[key][1])
         polygon_centroid_list[key] = [c_x, c_y]
     
     polygon_centroid_df = pd.DataFrame.from_dict(polygon_centroid_list, orient='index')
@@ -133,8 +144,10 @@ def generate_centroid_dataframe(df):
 osmhandler = OsmHandler()
 # scan the input file and fills the handler list accordingly
 PATH = "C:/Users/jack_minimonster/Documents/231n_dataset/BIC_GSV/data_query/osm_file/"
-filename = "georgia.osm.pbf"
+filename = "cal_north.osm.pbf"
 
+print('...reading osm file...')
+print()
 osmhandler.apply_file(PATH + filename, locations=True) # set location to True to get coordinate
 
 # extract building footprint information from osm and sort based on building id
@@ -143,24 +156,31 @@ data_colnames_way = ['type', 'id',
 df_osm_way = pd.DataFrame(osmhandler.osm_data_way, columns=data_colnames_way)
 df_osm_way = df_osm_way.sort_values(by=['type', 'id'])
 
+print('...construct osm dataframe...')
 df_osm_grouped = df_osm_way.groupby('building type')
 
+# loop over grouped osm file per building type and find centroid of each building footprint of this type
 for group_name, group_value in df_osm_grouped:
+    print('...get building group...')
     df_osm_building_group = df_osm_grouped.get_group(group_name)
-    polygon_centroid_df = generate_centroid_dataframe(df_osm_building_group)
-
+    # convert dataframe to dictionary
+    polygon_list_building_group = polygon_node_dataframe_to_dict(df_osm_building_group)
+    
     # randomly pick certain number of examples from the full building list
-    polygon_centroid_df_sample = polygon_centroid_df.sample(n=10)
-    csvname = filename[:-8] + '-' + str(group_name)
-
+    print('...randomly selecting building...')
+    num_sample = 400
+    if num_sample > len(polygon_list_building_group):
+        num_sample = len(polygon_list_building_group)
+    polygon_list_building_group_sample = dict(random.sample(polygon_list_building_group.items(), num_sample))
+    
+    # calculate centroid
+    print('...calculating centroid...')
+    polygon_centroid_df = generate_centroid_dataframe(polygon_list_building_group_sample)
+    
     # # save the centroid lat lon information as csv file
-    print('...saving csv - ' + csvname + '...')
-    polygon_centroid_df_sample.to_csv(csvname + '.csv')
+    csvname = filename[:-8] + '-' + str(group_name)
+    print('...saving csv: ' + csvname + '...')
+    polygon_centroid_df.to_csv(csvname + '.csv')
+    print()
 
-# csv_filename = 'building_centroid_coord.csv'
-# with open(csv_filename, 'w') as csv_file:
-#     for key in polygon_centroid_list_sample.keys():
-#         csv_file.write("%s,%s,%s\n"%(key,polygon_centroid_list_sample[key][0],
-#                                      polygon_centroid_list_sample[key][1]))
-
-# # output csv file in format 'building_id, [centroid lat x 1e7, centroid lon x 1e7]
+    # output csv file in format 'building_id, [centroid lat x 1e7, centroid lon x 1e7]
